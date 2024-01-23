69c701fd23a3e255e28adec4bfe6897962320272
shahriar021 <shahriarc19@gmail.com>
2024-01-23 21:12:44 +0600

    react native lesson

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+import Styles from './style';
+
+const App = () => {
+  return (
+    <View>
+      <Text style={Styles.textStyle}>hellow</Text>
+      <Text>hellow</Text>
+    </View>
+  );
+};
+
+export default App;
---
+++ style.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+
+const Styles = StyleSheet.create({
+  textStyle: {
+    fontSize: 50,
+    color: 'red',
+  },
+});
+
+export default Styles;
